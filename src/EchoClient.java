220acda73fa27acd7aadfb507fce0050cbe10d43
Angelo S. <boyangel90@hotmail.it>
2016-10-20 18:02:56 +0200

    Commit

--- src/EchoClient.java
+++ src/EchoClient.java
@@ -13,7 +13,6 @@ public class EchoClient {
 		BufferedReader br = new BufferedReader(new InputStreamReader(sock.getInputStream(),"UTF-8"));
 		String answer = br.readLine();
 		System.out.println("La stringa ricevuta dal server è: " + answer);
-		System.out.println("Angelo è una merdaccia!");
 		
 		sock.close();
 	}
