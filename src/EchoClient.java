e49e65181d21c16b9d88cb0d70c2acbcbe14bcc9
Angelo S. <boyangel90@hotmail.it>
2016-10-20 17:26:07 +0200

    First commit

---
+++ src/EchoClient.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.net.*;
+
+public class EchoClient {
+
+	public static void main(String[] args) throws UnknownHostException, IOException {
+		int port = 7777;
+		Socket sock = new Socket("localhost", port);
+		PrintWriter pw = new PrintWriter(new OutputStreamWriter(sock.getOutputStream(), "UTF-8"));
+		pw.println("Hello, world");
+		pw.flush();
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(sock.getInputStream(),"UTF-8"));
+		String answer = br.readLine();
+		System.out.println("La stringa ricevuta dal server è: " + answer);
+		
+		sock.close();
+	}
+
+}
